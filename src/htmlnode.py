a42fb09e8e7a840ca15e54bba4aa521f2a21552b
Artur <arthurbaghramian2003@gmail.com>
2025-01-30 14:23:27 +0400

    Add ParentNode class and text_node_to_html_node function to transform textnode to html by text_type

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -56,6 +56,25 @@ class HTMLNode:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
 
+class ParentNode(HTMLNode):
+
+    def __init__(self, tag, children: list, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("You must have a tag")
+        if not self.children:
+            raise ValueError("You must have children this is a ParentNode")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
+
+
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 
 class TextType(Enum):
@@ -23,7 +24,7 @@ class TextNode:
         url (str): The URL associated with the text node (optional).
     """
 
-    def __init__(self, text, text_type, url=None):
+    def __init__(self, text, text_type: "TextType", url=None):
         """
         Initializes a TextNode instance.
 
@@ -60,3 +61,19 @@ class TextNode:
             str: The string representation of the TextNode instance.
         """
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node: "TextNode"):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", "", {"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
